5978de09a0e041a35d26df7513a1266646c4dbbf
Mohsin Idrees <mohsinidrees2817@gmail.com>
2024-09-01 21:17:20 +0500

    commited

--- src/app.py
+++ src/app.py
@@ -1,23 +1,19 @@
 from datetime import datetime, timedelta, timezone
-
 import jwt
 import jwt.algorithms
-import streamlit as st  #all streamlit commands will be available through the "st" alias
+import streamlit as st  # all streamlit commands will be available through the "st" alias
 import utils
 from streamlit_feedback import streamlit_feedback
 import boto3
-UTC=timezone.utc
-
-
+UTC = timezone.utc
 
 # Init configuration
 utils.retrieve_config_from_agent()
 if "aws_credentials" not in st.session_state:
     st.session_state.aws_credentials = None
 
-st.set_page_config(page_title="Amazon Q Business Custom UI") #HTML title
-st.title("Amazon Q Business Custom UI") #page title
-
+st.set_page_config(page_title="Amazon Q Business Custom UI")  # HTML title
+st.title("Amazon Q Business Custom UI")  # page title
 
 # Define a function to clear the chat history
 def clear_chat_history():
@@ -29,34 +25,19 @@ def clear_chat_history():
     st.session_state["conversationId"] = ""
     st.session_state["parentMessageId"] = ""
 
-
 oauth2 = utils.configure_oauth_component()
+
+# Check if 'token' is not in session state
 if "token" not in st.session_state:
-    # If not, show authorize button
+    # Redirect directly if no token is present
     redirect_uri = f"https://{utils.OAUTH_CONFIG['ExternalDns']}/component/streamlit_oauth.authorize_button/index.html"
-    result = oauth2.authorize_button("Connect with Cognito",scope="openid", pkce="S256", redirect_uri=redirect_uri)
-    if result and "token" in result:
-        # If authorization successful, save token in session state
-        st.session_state.token = result.get("token")
-        # Retrieve the Identity Center token
-        st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(st.session_state.token["id_token"])
-        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + \
-            timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
-        st.rerun()
+    st.experimental_set_query_params(redirect=redirect_uri)
+    st.experimental_rerun()
 else:
     token = st.session_state["token"]
-    refresh_token = token["refresh_token"] # saving the long lived refresh_token
+    refresh_token = token["refresh_token"]  # saving the long-lived refresh_token
     user_email = jwt.decode(token["id_token"], options={"verify_signature": False})["email"]
 
-    # if st.button("Refresh Cognito Token") :
-    #     # If refresh token button is clicked or the token is expired, refresh the token
-    #     token = oauth2.refresh_token(token, force=True)
-    #     # Put the refresh token in the session state as it is not returned by Cognito
-    #     token["refresh_token"] = refresh_token
-    #     # Retrieve the Identity Center token
-
-    #     st.session_state.token = token
-    #     st.rerun()
     if "idc_jwt_token" not in st.session_state:
         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(token["id_token"])
         st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
@@ -66,17 +47,14 @@ else:
         try:
             st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(
                 st.session_state["idc_jwt_token"]["refreshToken"]
-                )
+            )
             st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
                 timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
         except Exception as e:
             st.error(f"Error refreshing Identity Center token: {e}. Please reload the page.")
-    
-    # col1, col2 = st.columns([1,1])
-    # with col1:
-    #     st.write("Welcome: ", user_email)
+
     user_name = jwt.decode(token["id_token"], options={"verify_signature": False})["cognito:username"]
-    st.sidebar.text ("Welcome: " + user_name)
+    st.sidebar.text("Welcome: " + user_name)
     st.markdown(
         f"""
          <style>
@@ -91,12 +69,9 @@ else:
         """,
         unsafe_allow_html=True
     )
-    # st.write("token: ", token)
-    # st.write("idc_jwt_token: ", st.session_state["idc_jwt_token"])
-    # st.write("refresh_token: ", refresh_token)
-    # st.write("aws_credentials: ", st.session_state.aws_credentials)
+
     if st.sidebar.button("logout"):
-        utils.logout()   
+        utils.logout()
     st.button("Clear Chat History", on_click=clear_chat_history)
 
     # Initialize the chat messages in the session state if it doesn't exist
@@ -121,19 +96,16 @@ else:
     if "input" not in st.session_state:
         st.session_state.input = ""
 
-
     # Display the chat messages
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
-
 
     # User-provided prompt
     if prompt := st.chat_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.write(prompt)
-
 
     # Check if the last message is not from the assistant and messages list is not empty
     if st.session_state.messages and st.session_state.messages[-1]["role"] != "assistant":
@@ -152,4 +124,3 @@ else:
                 st.session_state["parentMessageId"] = response["parentMessageId"]
 
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-        
